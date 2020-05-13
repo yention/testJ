6aa55ddb9e855cda0795daa9b0fa62c3d5b03934
Aliaksandr Yenza <sashayenza1996@gmail.com>
2020-05-13 12:42:12 -0400

    first commit

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+    @Test
+    public void testName00001() {
+
+    }
+
+    @Test
+    public void testNameForFuckingUrName() {
+    }
+}
